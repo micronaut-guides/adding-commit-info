1b18eacddd3cf53a1a4db5f2d1077951d85d5442
Iván López <lopezi@objectcomputing.com>
2019-11-08 11:53:59 +0100

    Use @MicronautTest

--- complete/src/test/java/example/micronaut/InfoTest.java
+++ complete/src/test/java/example/micronaut/InfoTest.java
@@ -1,59 +1,39 @@
 package example.micronaut;
 
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
-import io.micronaut.runtime.server.EmbeddedServer;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
 import java.util.Map;
 
-import static junit.framework.TestCase.assertNotNull;
-import static org.junit.Assert.assertEquals;
-
+@MicronautTest // <1>
 public class InfoTest {
-    private static EmbeddedServer server;
-    private static HttpClient client;
 
-    @BeforeClass
-    public static void setupServer() {
-        server = ApplicationContext.run(EmbeddedServer.class); // <1>
-        client = server
-                .getApplicationContext()
-                .createBean(HttpClient.class, server.getURL()); // <2>
-    }
-
-    @AfterClass
-    public static void stopServer() {
-        if(server != null) {
-            server.stop();
-        }
-        if(client != null) {
-            client.stop();
-        }
-    }
+    @Inject
+    @Client("/")
+    HttpClient client; // <2>
 
     @Test
     public void testGitComitInfoAppearsInJson() {
-
         HttpRequest request = HttpRequest.GET("/info"); // <3>
 
         HttpResponse<Map> rsp = client.toBlocking().exchange(request, Map.class);
 
-        assertEquals(200, rsp.status().getCode());
+        Assertions.assertEquals(200, rsp.status().getCode());
 
         Map json = rsp.body(); // <4>
 
-        then:
-        assertNotNull(json.get("git"));
-        assertNotNull(((Map) json.get("git")).get("commit"));
-        assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("message"));
-        assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("time"));
-        assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("id"));
-        assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("user"));
-        assertNotNull(((Map) json.get("git")).get("branch"));
+        Assertions.assertNotNull(json.get("git"));
+        Assertions.assertNotNull(((Map) json.get("git")).get("commit"));
+        Assertions.assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("message"));
+        Assertions.assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("time"));
+        Assertions.assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("id"));
+        Assertions.assertNotNull(((Map) ((Map) json.get("git")).get("commit")).get("user"));
+        Assertions.assertNotNull(((Map) json.get("git")).get("branch"));
     }
 }
